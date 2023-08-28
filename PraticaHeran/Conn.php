5cdc3ff736354c2ff07d87aff76338e4dfd31929
Luiz Rodrigues <luizrodriguescode@gmail.com>
2023-08-28 14:52:02 -0300

    terceiro commit

--- PraticaHeran/Conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Conn{
-        public string $host = "localhost";
-        public string $dbname = "phe";
-        public string $user = "root";
-        public string $pass = "";
-        # 
-    }
